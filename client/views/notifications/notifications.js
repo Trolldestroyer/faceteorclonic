21de4723c404b73ad3d1186612290e591aa78fc3
Trolldestroyer <kisukemi@gmail.com>
2017-04-26 18:36:11 +0200

    my first commit

--- client/views/notifications/notifications.js
+++ client/views/notifications/notifications.js
@@ -1,9 +1,13 @@
 import { Session } from 'meteor/session'
-
-Template.notifications.onCreated(function(){
-
-})
-
+/**
+ * @isMethod true
+ * @memberOf Method
+ * @function events
+ * @summary metodo con el cual usamos el evento de accept o refuse para aceptar o rechazar la peticion de amistad.
+ * @locus notifications
+ * @param {Object} [clickdUser] Guardamos el usuario que a clicado para aceptar o rechazar. 
+ * @param {Object} [userid] Es dondo guardamos la id del usuario.
+ */
 Template.notifications.events({
     'click .accept':function(e){
       var clickedUser = Blaze.getData(e.currentTarget);
@@ -19,12 +23,27 @@ Template.notifications.events({
 })
 
 Template.notifications.helpers({
+  /**
+ * @isMethod true
+ * @memberOf Method
+ * @function friendRequests
+ * @summary Esta function nos retorna los usuarios que espera que respondas a la solicitud.
+ * @locus notifications
+ * @param {Object} [user] Es dondo guardamos el usuario que quiere saber sus solicitedes.
+ */
     friendRequests:function(){
         var user = Meteor.user();
-        console.log(user);
-        console.log(user.profile.pendingFriends);
         return user.profile.pendingFriends;
     },
+ /**
+ * @isMethod true
+ * @memberOf Method
+ * @function friendPicture
+ * @summary Esta function nos retorna la pic del friend.
+ * @locus notifications
+ * @param {Object} [friendpic] Es dondo guardamos el nombre modificado del amigo introducido añadientole pic al nombre.
+ * @param {Object} [username] Es dondo guardamos y modificamos la session para despues obtener la pic.
+ */
     friendPicture:function(friend) {
       var friendpic = friend+'_pic';
       var username =  Meteor.call('users.friendsrc',friend, function(error, result) {
@@ -33,10 +52,26 @@ Template.notifications.helpers({
 
       return Session.get(friendpic);
     },
+  /**
+ * @isMethod true
+ * @memberOf Method
+ * @function friendName
+ * @summary Esta function nos retorna el usuario con la id solicitada.
+ * @locus notifications
+ * @param {Object} [user] Es dondo guardamos el usuario amigo solicitado.
+ */
     friendName:function(friend) {
         var user = Meteor.users.findOne({_id:friend});
         return user ? user.profile.name.first + " " + user.profile.name.last : null;
     },
+    /**
+ * @isMethod true
+ * @memberOf Method
+ * @function friendAddress
+ * @summary Esto nos retorna la inforacion de localizacion de la persona.
+ * @locus notifications
+ * @param {Object} [user] Es dondo guardamos el usuario amigo del que queremos saber la localizacion.
+ */
     friendAddress:function(friend, address) {
         var user = Meteor.users.findOne({_id:friend});
         switch (address) {
@@ -48,6 +83,14 @@ Template.notifications.helpers({
                 break;
         }
     },
+ /**
+ * @isMethod true
+ * @memberOf Method
+ * @function friendUsername
+ * @summary Esto nos retorna el nombre del amigo.
+ * @locus notifications
+ * @param {Object} [user] Es dondo guardamos el nombre del usuario amigo añadiendo _name.
+ */
     friendUsername:function(friend) {
       var friendname = friend+'_name';
       Meteor.call('users.FriendName',friend, function(error, result) {
@@ -56,14 +99,4 @@ Template.notifications.helpers({
       });
       return Session.get(friendname);
    }
-    // friendUsername:function(friend) {
-    //     var friendID = "'"+friend+"'";
-    //     var username =  Meteor.call('users.FriendName',friend, function(error, result) {
-    //         // 'result' is the method return value
-    //         Session.set('result', serverResult);
-    //         return result;
-    //     });
-    //     console.log("Estoy aquí"+username);
-    //     return username;
-    // }
 })
